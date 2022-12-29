7daa5896ed43a6abf2fcd01557263dfff364ee88
Rajat Kumar Prajapati <58185478+itsRajatkumar@users.noreply.github.com>
2022-12-29 09:36:12 +0000

    fixed click comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useRef,useState } from "react";
 import OutsideClickDetect from "./components/click-outside-detect";
+// import OutsideClickDetect from "click-outside-detect";
 
 function App() {
   const [click, setClick] = useState(false)
   const div1Ref = useRef();
   const changeColor = ()=>{
+    console.log("click here")
     setClick(true)
     setTimeout(() => {
       setClick(false)
@@ -14,10 +16,10 @@ function App() {
   OutsideClickDetect(div1Ref,changeColor)
   return (
     <div style={{height:"100vh",width:"100vw", display:"flex",justifyContent:"space-between", alignItem:"center"}}>
-      <div ref={div1Ref} style={{height:"100vh",width:"40vw", display:"flex",justifyContent:"space-between", alignItem:"center"}}>
+      <div style={{height:"100vh",width:"40vw", display:"flex",justifyContent:"space-between", alignItem:"center"}}>
         click here
       </div>
-      <div style={{height:"100vh",width:"40vw", display:"flex",justifyContent:"space-between", alignItem:"center"}}>
+      <div ref={div1Ref} style={{height:"100vh",width:"40vw", display:"flex",flexDirection:"column",justifyContent:"space-between", alignItem:"center"}}>
         Div 2
         {click && <h1>Click out side</h1>}
       </div>
